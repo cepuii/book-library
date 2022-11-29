0a616fd5f877512281c557f221ee26dae7cd80a6
cepuii <cepuii13@gmail.com>
2022-11-29 17:11:38 +0200

    Add database properties and configure ConnectionPool

---
+++ src/main/java/ua/od/cepuii/library/config/ConnectionPool.java
@@ -0,0 +1,32 @@
+package ua.od.cepuii.library.config;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class ConnectionPool {
+    private static final HikariConfig config = new HikariConfig();
+    private static final HikariDataSource ds;
+
+    static {
+
+        config.setJdbcUrl("jdbc:postgresql://localhost:5432/book_library");
+        config.setUsername("postgres");
+        config.setPassword("postgres");
+        config.setDriverClassName("org.postgresql.Driver");
+        config.addDataSourceProperty("cachePrepStmts", "true");
+        config.addDataSourceProperty("prepStmtCacheSize", "250");
+        config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
+        ds = new HikariDataSource(config);
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return ds.getConnection();
+    }
+
+    private ConnectionPool() {
+    }
+}
+
