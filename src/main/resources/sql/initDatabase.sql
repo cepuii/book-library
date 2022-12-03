7d0fcc6317dd3f028dd57eb8662b44a14da28d51
cepuii <cepuii13@gmail.com>
2022-12-03 19:14:38 +0200

    Change initDb
    Add new table type of publication
    And change reletions

--- src/main/resources/sql/initDatabase.sql
+++ src/main/resources/sql/initDatabase.sql
@@ -1,4 +1,5 @@
-DROP TABLE IF EXISTS user_role;
+DROP TABLE IF EXISTS user_role CASCADE;
+DROP TABLE IF EXISTS publication_type CASCADE;
 DROP TABLE IF EXISTS loan;
 DROP TABLE IF EXISTS book_author;
 DROP TABLE IF EXISTS author;
@@ -6,16 +7,23 @@ DROP TABLE IF EXISTS book;
 DROP TABLE IF EXISTS users;
 DROP SEQUENCE IF EXISTS global_seq;
 
-CREATE SEQUENCE global_seq START WITH 100;
+CREATE SEQUENCE global_seq START WITH 1000;
+
+CREATE TABLE publication_type
+(
+    id   INTEGER PRIMARY KEY NOT NULL,
+    type VARCHAR             NOT NULL
+);
 
 CREATE TABLE book
 (
     id               BIGINT PRIMARY KEY DEFAULT nextval('global_seq'),
-    title            VARCHAR                        NOT NULL,
-    publication      VARCHAR                        NOT NULL,
-    date_publication INTEGER                        NOT NULL,
-    total            INTEGER                        NOT NULL,
-    no_actual        INTEGER            default '0' NOT NULL
+    title            VARCHAR NOT NULL,
+    publication_id   INTEGER NOT NULL,
+    date_publication INTEGER NOT NULL,
+    total            INTEGER NOT NULL,
+
+    FOREIGN KEY (publication_id) REFERENCES publication_type (id) ON DELETE CASCADE
 );
 
 CREATE UNIQUE INDEX books_unique_title_idx ON book (title);
@@ -30,9 +38,16 @@ CREATE TABLE book_author
 (
     book_id   INTEGER,
     author_id INTEGER,
+    PRIMARY KEY (book_id, author_id),
+
     FOREIGN KEY (book_id) REFERENCES book (id) ON DELETE CASCADE,
-    FOREIGN KEY (author_id) REFERENCES author (id) ON DELETE CASCADE,
-    PRIMARY KEY (book_id, author_id)
+    FOREIGN KEY (author_id) REFERENCES author (id) ON DELETE CASCADE
+);
+
+CREATE TABLE user_role
+(
+    id   INTEGER PRIMARY KEY NOT NULL,
+    role VARCHAR             NOT NULL
 );
 
 CREATE TABLE users
@@ -42,28 +57,25 @@ CREATE TABLE users
     password   VARCHAR                          NOT NULL,
     registered TIMESTAMP          DEFAULT now() NOT NULL,
     blocked    BOOLEAN            DEFAULT false NOT NULL,
-    fine       INTEGER            DEFAULT 0     NOT NULL
+    role_id    BIGINT             DEFAULT 1     NOT NULL,
+
+    FOREIGN KEY (role_id) REFERENCES user_role (id) ON DELETE CASCADE
 );
 
 CREATE UNIQUE INDEX users_unique_email_idx ON users (email);
 
-CREATE TABLE user_role
-(
-    user_id INTEGER NOT NULL,
-    role    VARCHAR NOT NULL,
-    CONSTRAINT user_role_idx UNIQUE (user_id, role),
-    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
-);
 
 
 CREATE TABLE loan
 (
-    id         SERIAL PRIMARY KEY,
-    user_id    INTEGER                 NOT NULL,
-    book_id    INTEGER                 NOT NULL,
-    start_time DATE    DEFAULT now()   NOT NULL,
-    duration   INTEGER DEFAULT ('0')   NOT NULL,
-    status     VARCHAR DEFAULT ('RAW') NOT NULL,
+    id         BIGINT PRIMARY KEY DEFAULT nextval('global_seq'),
+    user_id    INTEGER                            NOT NULL,
+    book_id    INTEGER                            NOT NULL,
+    start_time DATE               DEFAULT now()   NOT NULL,
+    duration   INTEGER            DEFAULT ('0')   NOT NULL,
+    status     VARCHAR            DEFAULT ('RAW') NOT NULL,
+    fine       INTEGER            DEFAULT 0       NOT NULL,
+
     FOREIGN KEY (user_id) REFERENCES users (id),
     FOREIGN KEY (book_id) REFERENCES book (id)
 );
