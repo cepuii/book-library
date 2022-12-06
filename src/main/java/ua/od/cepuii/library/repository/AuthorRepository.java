30ca90d8609b55303151bb654457911f95725f5f
cepuii <cepuii13@gmail.com>
2022-12-06 21:10:30 +0200

    Create AuthorRepository class and implement it

--- src/main/java/ua/od/cepuii/library/repository/AuthorRepository.java
+++ src/main/java/ua/od/cepuii/library/repository/AuthorRepository.java
@@ -2,9 +2,6 @@ package ua.od.cepuii.library.repository;
 
 import ua.od.cepuii.library.model.Author;
 
-import java.util.Set;
-
 public interface AuthorRepository extends AbstractEntityRepository<Author> {
 
-    Set<Author> getAuthorsByBook();
 }
