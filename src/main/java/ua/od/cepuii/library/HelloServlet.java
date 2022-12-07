7fff592ca8baec60f68e553481869dfc3f39e7d9
Serhiy Chernousov <cepuii13@gmail.com>
2022-12-07 19:31:30 +0200

    Fix error after code refactoring

--- src/main/java/ua/od/cepuii/library/HelloServlet.java
+++ src/main/java/ua/od/cepuii/library/HelloServlet.java
@@ -4,7 +4,8 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import ua.od.cepuii.library.config.ConnectionPool;
+import ua.od.cepuii.library.db.ConnectionPool;
+import ua.od.cepuii.library.db.HikariConnectionPool;
 import ua.od.cepuii.library.model.Role;
 import ua.od.cepuii.library.model.User;
 
@@ -17,39 +18,42 @@ import java.sql.Statement;
 
 @WebServlet(name = "helloServlet", value = "/hello-servlet")
 public class HelloServlet extends HttpServlet {
-    private String message;
-
-    @Override
-    public void init() {
-        message = "Hello stupid World!";
+  
+  private String message;
+  private ConnectionPool connectionPool = new HikariConnectionPool();
+  
+  @Override
+  public void init() {
+    message = "Hello stupid World!";
+  }
+  
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    response.setContentType("text/html");
+    User user = null;
+    try (Connection connection = connectionPool.getConnection();
+        Statement statement = connection.createStatement()) {
+      ResultSet resultSet = statement.executeQuery("SELECT * FROM \"user\"");
+      resultSet.next();
+      user = new User(resultSet.getInt(1), resultSet.getString(2),
+          resultSet.getString(3),
+          resultSet.getTimestamp(4).toLocalDateTime(), resultSet.getBoolean(5),
+          resultSet.getInt(6), Role.values()[resultSet.getInt("role_id")]);
+      
+    } catch (SQLException e) {
+      e.printStackTrace();
     }
-
-    @Override
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setContentType("text/html");
-        User user = null;
-        try (Connection connection = ConnectionPool.getConnection();
-             Statement statement = connection.createStatement()) {
-            ResultSet resultSet = statement.executeQuery("SELECT * FROM \"user\"");
-            resultSet.next();
-            user = new User(resultSet.getInt(1), resultSet.getString(2),
-                    resultSet.getString(3), resultSet.getString(4),
-                    resultSet.getTimestamp(5).toLocalDateTime(), resultSet.getBoolean(6),
-                    resultSet.getInt(7), Role.UNREGISTER);
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        // Hello
-        PrintWriter out = response.getWriter();
-        out.println("<html><head><link href=\"resources/css/style.css\" rel=\"stylesheet\"></head><body>");
-        out.println("<h1>" + message + "</h1>");
-        out.println(user);
-        out.println("</body></html>");
-    }
-
-    @Override
-    public void destroy() {
-        throw new UnsupportedOperationException();
-    }
+    // Hello
+    PrintWriter out = response.getWriter();
+    out.println(
+        "<html><head><link href=\"resources/css/style.css\" rel=\"stylesheet\"></head><body>");
+    out.println("<h1>" + message + "</h1>");
+    out.println(user);
+    out.println("</body></html>");
+  }
+  
+  @Override
+  public void destroy() {
+    throw new UnsupportedOperationException();
+  }
 }
